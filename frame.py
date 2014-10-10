9736ad97754b4a1cea5b12137779d61885e9dbfe
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2014-10-10 16:40:55 +0200

    some stuff

--- frame.py
+++ frame.py
@@ -4,7 +4,26 @@ class Frame:
 
     def __init__(self, this=None, parameters=[], max_stack=0, max_locals=0):
         self.stack = []
-        self.local_variables = [this] + parameters
+        self.local_variables = [None] * max_locals
+        i=0
+        if this:
+            self.local_variables[i] = this
+            i+=1
+        for param in parameters:
+            self.local_variables[i] = param
+            i += 1
         self.max_stack=max_stack
         self.max_locals=max_locals
 
+    def push(self, value):
+        self.stack.append(value)
+        assert len(self.stack) <= self.max_stack
+
+    def pop(self):
+        return self.stack.pop()
+
+    def insert_local(self, index, value):
+        self.local_variables[index] = value
+
+    def get_local(self, index):
+        return self.local_variables[index]
--- vm.py
+++ vm.py
@@ -2,6 +2,10 @@ from utils import get_attribute
 from defaultclassloader import DefaultClassLoader
 from frame import Frame
 from klass import NoSuchMethodException
+from classconstants import ACC_STATIC
+
+null = object()
+void = object()
 
 class VM:
     def __init__(self, classpath=[]):
@@ -38,22 +42,41 @@ class VM:
         print 'running method', method
         # yup, our stack has infinite depth. Contains only frames
         code = get_attribute(method, 'Code')
-        frame = Frame(max_stack=code.max_stack, max_locals=code.max_locals)
+
+        if method.access_flags | ACC_STATIC:
+            this = None
+        else:
+            this = None
+        frame = Frame(this=this, max_stack=code.max_stack, max_locals=code.max_locals)
         self.stack.append(frame)
 
-        self.run_bytecode(code.code, frame)
+        return_value = self.run_bytecode(method, code.code, frame)
 
         # TODO: check for frame return value somehow
 
         self.stack.pop()
+        return return_value
 
-    def run_bytecode(self, bytecode, frame):
+    def run_bytecode(self, method, bytecode, frame):
         pc = 0
         while pc < len(bytecode):
-            raise Exception('Unknown bytecode: %d' % bytecode[pc])
+            bc = bytecode[pc]
+            # iconst_<n>
+            if 2 <= bc <= 8:
+                frame.push(bc-2-1)
+            # istore_<n>
+            elif 59 <= bc <= 62:
+                frame.insert_local(bc - 59, frame.pop())
             # aload_<n>
-            if 42 <= bytecode[pc] <= 45:
-                n = 45 - bytecode[pc]
+            elif 42 <= bc <= 45:
+                n = 45 - bc
                 frame.stack.push(frame.local_variables[n])
+            # return
+            elif bc == 177:
+                # TODO: parse out the return type and assert void
+                # TODO: if synchronized method exit the monitor
+                return void
+            else:
+                raise Exception('Unknown bytecode: %d' % bytecode[pc])
             pc += 1
 
