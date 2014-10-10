25369053949af5980d51506e9c6e38a52c99a64c
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2014-10-10 22:05:14 +0200

    descriptor throws errors when malformed, handles array type

--- descriptor.py
+++ descriptor.py
@@ -1,15 +1,16 @@
 NORMAL = 1
 PARAMETERS = 2
+ARRAY = 3
 
 class MalformedDescriptorException(Exception):
     pass
 
-def parse_descriptor(string, mode=NORMAL):
+def _parse_descriptor(string, mode=NORMAL):
     result = []
     index = 0
     while index < len(string):
         if string[index] == '(' and mode == NORMAL:
-            result.append(parse_descriptor(string[index+1:string.find(')', index)], PARAMETERS))
+            result.append(_parse_descriptor(string[index+1:string.find(')', index)], PARAMETERS)[0])
             index = string.find(')', index)+1
         elif string[index] in 'BCDFIJSZV':
             result.append(string[index])
@@ -17,11 +18,23 @@ def parse_descriptor(string, mode=NORMAL):
         elif string[index] == 'L':
             result.append(string[index+1:string.find(';', index+1)])
             index = string.find(';', index+1)+1
+        elif string[index] == '[':
+            array_type, offset = _parse_descriptor(string[index+1:], ARRAY)
+            assert len(array_type) == 1
+            result.append('['+array_type[0])
+            index += offset+1
+        else:
+            raise MalformedDescriptorException('Unknown character %s' % string[index])
+        if mode == ARRAY:
+            break
 
     if mode == NORMAL and len(result) not in (1, 2):
         raise MalformedDescriptorException()
 
-    return result
+    return result, index
+
+def parse_descriptor(string):
+    return _parse_descriptor(string)[0]
 
 if __name__ == '__main__':
-    print parse_descriptor('(IDLjava/lang/Thread;)Ljava/lang/Object;')
+    print parse_descriptor('(I[D[[[BLjava/lang/Thread;)Ljava/lang/Object;')
